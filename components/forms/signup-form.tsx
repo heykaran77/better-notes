a751e8916720f00e813418a5a2ae3176dd62bb67
Karan Singh <devkaran.codes@gmail.com>
2026-01-07 00:00:02 +0530

    feat: add signup form component with validation and UI elements

--- components/forms/signup-form.tsx
+++ components/forms/signup-form.tsx
@@ -8,17 +8,10 @@ import {
   FieldLabel,
   FieldSeparator,
 } from "@/components/ui/field";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import Link from "next/link";
 import GithubSVG from "@/components/logos/GithubSVG";
 
@@ -48,62 +41,90 @@ export function SignupForm({
   function onSubmit(values: z.infer<typeof signUpSchema>) {}
 
   return (
-    <Form {...form}>
-      <FieldGroup>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-          <div className="flex flex-col items-center gap-1 text-center">
-            <h1 className="text-2xl font-bold">Create your account</h1>
-            <p className="text-muted-foreground text-sm text-balance">
-              Fill in the form below to create your account
-            </p>
-          </div>
-          <FormField
-            control={form.control}
-            name="name"
-            render={({ field, fieldState }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </FormItem>
-            )}
-          />
-          <Field>
-            <FieldLabel htmlFor="email">Email</FieldLabel>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </Field>
-          <Field>
-            <FieldLabel htmlFor="password">Password</FieldLabel>
-            <Input id="password" type="password" required />
-          </Field>
-          <Field>
-            <FieldLabel htmlFor="confirm-password">Confirm Password</FieldLabel>
-            <Input id="confirm-password" type="password" required />
-          </Field>
-          <Field>
-            <Button type="submit">Create Account</Button>
-          </Field>
-          <FieldSeparator>Or continue with</FieldSeparator>
-          <Field>
-            <Button variant="outline" type="button">
-              <GithubSVG />
-              Sign up with GitHub
-            </Button>
-            <FieldDescription className="px-6 text-center">
-              Already have an account? <Link href="/auth/login">Sign in</Link>
-            </FieldDescription>
-          </Field>
-        </form>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+      <div className="flex flex-col items-center gap-1 text-center">
+        <h1 className="text-2xl font-bold">Create your account</h1>
+        <p className="text-muted-foreground text-sm text-balance">
+          Fill in the form below to create your account
+        </p>
+      </div>
+      <FieldGroup className="space-y-px">
+        <Controller
+          name="email"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Email</FieldLabel>
+              <Input
+                type="email"
+                placeholder="johndoe@mail.com"
+                aria-invalid={fieldState.invalid}
+                {...field}
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Controller
+          name="name"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Name</FieldLabel>
+              <Input
+                type="text"
+                {...field}
+                aria-invalid={fieldState.invalid}
+                placeholder="name"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Controller
+          name="password"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Password</FieldLabel>
+              <Input
+                type="password"
+                {...field}
+                aria-invalid={fieldState.invalid}
+                placeholder="********"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Controller
+          name="password"
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field>
+              <FieldLabel>Confirm Password</FieldLabel>
+              <Input
+                type="password"
+                {...field}
+                aria-invalid={fieldState.invalid}
+                placeholder="********"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+        <Button type="submit">Create Account</Button>
+        <FieldSeparator>Or continue with</FieldSeparator>
+        <Field>
+          <Button variant="outline" type="button">
+            <GithubSVG />
+            Sign up with GitHub
+          </Button>
+          <FieldDescription className="px-6 text-center">
+            Already have an account? <Link href="/auth/login">Sign in</Link>
+          </FieldDescription>
+        </Field>
       </FieldGroup>
-    </Form>
+    </form>
   );
 }
